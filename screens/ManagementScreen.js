bd2a70efe075785215801308af14a8c17629c5c9
Teddy Pascual <teddylayson2000@gmail.com>
2025-09-14 12:56:25 +0800

    Implement modal for adding employees with input fields for name and rate in ManagementScreen

--- screens/ManagementScreen.js
+++ screens/ManagementScreen.js
@@ -1,9 +1,14 @@
 import { View, StyleSheet } from "react-native";
 import Divider from "react-native-paper/src/components/Divider";
 import ListIconItem from "../components/ListIconItem";
-import { Text } from "react-native-paper";
 import FilledButton from "../components/FilledButton";
-import { Modal, Portal, Button, PaperProvider } from "react-native-paper";
+import {
+  Modal,
+  Portal,
+  Text,
+  PaperProvider,
+  TextInput,
+} from "react-native-paper";
 import { useState } from "react";
 const ManagementScreen = () => {
   const data = {
@@ -22,17 +27,25 @@ const ManagementScreen = () => {
     { id: 2, name: "Neggy", position: "Helper", rate: 250 },
   ];
   const [visible, setVisible] = useState(false);
-
+  const [empName, setEmpName] = useState("");
+  const [empRate, setEmpRate] = useState("");
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
   const containerStyle = {
     backgroundColor: "white",
     width: "90%",
-
+    borderRadius: 8,
+    padding: 20,
     height: "50%",
+    flexDirection: "column",
+    gap: 8,
     marginHorizontal: "auto",
   };
-
+  function addEmployee() {
+    setEmpName("");
+    setEmpRate("");
+    hideModal();
+  }
   return (
     <PaperProvider>
       <View style={styles.container}>
@@ -52,11 +65,23 @@ const ManagementScreen = () => {
             <Text variant="headlineSmall" style={{ textAlign: "center" }}>
               Add Employee
             </Text>
+            <TextInput
+              label="Employee Name"
+              value={empName}
+              onChangeText={(text) => setEmpName(text)}
+            />
+            <TextInput
+              label="Employee Rate"
+              value={empRate}
+              onChangeText={(text) => setEmpRate(text)}
+              keyboardType="numeric"
+              inputMode="numeric"
+            />
             <FilledButton
               icon={"account-plus"}
               text={"Add Employee"}
               size={"90%"}
-              onPress={hideModal}
+              onPress={() => addEmployee()}
             />
           </Modal>
         </Portal>
